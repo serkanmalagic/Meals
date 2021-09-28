47a1a3460abf1ec7a8354daf4f993f8b4b39a66f
Serkan Mehmet MALAGİÇ <serkanmalagic@hotmail.com>
2021-09-28 14:39:48 +0300

    api url added

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,12 +1,12 @@
-import classes from './MealsSummary.module.css';
+import classes from "./MealsSummary.module.css";
 
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
+      <h2>O kadar lezzetli ki hesap sormayı unutacaksınız</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor
+        Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor
       </p>
       <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
